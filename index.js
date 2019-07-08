4b42ecf4c860fe13b86da5c000324b1d3bde2a14
elipmoc <relipmoc101@gmail.com>
2019-07-09 00:57:04 +0900

    add name

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-class Arutemyan {
+const Name = require('./lib/name')
 
+class Arutemyan {
+    // あるてみかんを文字列で取得
+    get hiraganaName() { return Name.getHiraganaName(); }
+    // arutemyanを文字列で取得
+    get name() { return Name.getName(); }
 }
 
 module.exports = Arutemyan;
--- tests/arutemyan.test.js
+++ tests/arutemyan.test.js
@@ -3,3 +3,9 @@ const Arutemyan = require('../index');
 test('new Arutemyan', () => {
     expect(new Arutemyan()).toBeDefined();
 })
+
+test('get Arutemyan name', () => {
+    const arutemyan = new Arutemyan();
+    expect(arutemyan.hiraganaName).toBe("あるてみかん");
+    expect(arutemyan.name).toBe("arutemyan");
+})
